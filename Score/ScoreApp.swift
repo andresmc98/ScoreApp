7ec3065be9bf8c7196802615b4949f35b0f20951
andresmc98 <jossean98@gmail.com>
2025-08-29 19:31:02 -0700

    Initial Commit

---
+++ Score/ScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreApp.swift
+//  Score
+//
+//  Created by Mac on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScoreTests/ScoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreTests.swift
+//  ScoreTests
+//
+//  Created by Mac on 29/08/25.
+//
+
+import Testing
+@testable import Score
+
+struct ScoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
